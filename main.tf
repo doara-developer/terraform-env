7cd6621d94dbdfb2ef7c870961c3ac3bc599fe8e
doara-developer <doaradev@gmail.com>
2020-12-17 22:35:42 +0900

    Add s3 bucket for manage tfstate

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_version = ">= 0.11.0"
+  backend "s3" {
+    bucket  = "terraform-sample-332ec6ba-ddc3-40fb-9089-7a35458631ca"
+    region  = "ap-northeast-1"
+    key     = "terraform.tfstate"
+    encrypt = true
+  }
+}
 provider "aws" {
   region = "ap-northeast-1"
 }
@@ -7,5 +16,12 @@ resource "aws_vpc" "main" {
 
   tags = {
     Name = "terraform-sample"
+  }
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-sample-332ec6ba-ddc3-40fb-9089-7a35458631ca"
+  versioning {
+    enabled = true
   }
 }
